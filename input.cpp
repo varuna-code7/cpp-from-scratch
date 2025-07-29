c58dc14fb92db31127cb5c9caeb02f45a4f59711
varuna-code7 <muthuvaruna4@gmail.com>
2025-07-29 22:53:12 +0530

    input.cpp added

--- input.cpp
+++ input.cpp
@@ -1,1 +1,11 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"enter value a:";
+    cout<<"enter value b";
+    cin>>a>>b;
+    cout<<"total"<<a+b;
+    return 0;
